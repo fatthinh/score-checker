6f90a1cacfc2461b44b5319909cbfd8aab811ec5
fatthinh <pthinh.lama@gmail.com>
2025-01-26 07:16:09 +0700

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const API_BASE_URL = 'http://52.221.193.90:8080/seventee/api/';
-const API_BASE_URL = "http://localhost:8000/app/";
+const API_BASE_URL = "https://lpthinh-sparta.site/app/";
 
 const baseConfig = {
   baseURL: API_BASE_URL,
