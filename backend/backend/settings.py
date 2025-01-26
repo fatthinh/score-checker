154f055507570124ebb6da7aa4ed59cdfa2dcdc0
fatthinh <pthinh.lama@gmail.com>
2025-01-26 07:13:44 +0700

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,6 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://103.137.184.129:6379/1",
     }
 }
